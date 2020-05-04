a459b94ab9246ee3764732728ea9a4e434d4560c
Vadim Balan <vbalan@mail.greenriver.edu>
2020-05-04 17:36:33 -0400

    Added more examples of using arrays

--- index.php
+++ index.php
@@ -14,10 +14,21 @@ require_once("vendor/autoload.php");
 $f3 = Base::instance();
 
 // Default route
-$f3->route('GET /', function()
+$f3->route('GET /', function($f3)
 {
     //echo '<h1>Week 5</h1>';
 
+    // Create variables in the hive
+    $f3->set('username', 'jshmo');
+    $f3->set('password', sha1('Password01'));
+    $f3->set('title', 'Working with Templates');
+    $f3->set('temp', 67);
+    $f3->set('color', "purple");
+    $f3->set('radius', '10');
+    $f3->set('bookmarks', array('http://ww.cnet.com', 'http://www.reddit.com/r/news', 'http://edition.cnn.com/sport'));
+    $f3->set('desserts', array("chocolate"=>"Chocolate Mousse", "vanilla"=>"Vanilla Custard", "strawberry"=>"Strawberry Shortcake"));
+
+    // Display template
     $view = new Template();
     echo $view->render('views/info.html');
 });
